3a72a3c5dde665838fc1933fda7c6d3239217fd3
Jose Ayerdis <jose.ayerdis@booking.com>
2013-06-25 00:46:21 +0200

    Remover archivo inutiles de esta rama

--- src/info/denisjtorresg/wsproxy/bcn/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://servicios.bcn.gob.ni/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package info.denisjtorresg.wsproxy.bcn;
